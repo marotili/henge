f86b82c5418c4d2c06c8fd11d3be3926479edc7e
Marco Fruhwirth <fruhwirth.marco@gmail.com>
2014-08-13 15:11:10 +0200

    added update script

---
+++ update.sh
@@ -0,0 +1,21 @@
+git pull origin master
+cd OpenGL
+git pull mfpi master
+cd ../OpenGLRaw
+git pull mfpi master
+cd ../spacepart
+git pull origin master
+cd ../freetype2
+git pull origin cabalization
+cd ../halo
+git pull origin master
+cd ../hsys
+git pull origin master
+cd ../imagemagick
+git pull mfpi master
+cd ../games/boom
+git pull origin master
+
+cabal clean
+cabal configure
+cabal build boom
